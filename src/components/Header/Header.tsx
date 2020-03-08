67966345e4ee06a0ec5afeac79dbdab454526be0
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2020-03-08 18:25:12 +0100

    feat(Header): Header of the amiga os window

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,19 +1,14 @@
 import React from 'react';
-import { Link } from 'react-router-dom';
 import { default as bemCssModules } from 'bem-css-modules';
 import { default as HeaderStyles } from './Header.module.scss';
 
 const style = bemCssModules(HeaderStyles);
 
 export const Header: React.FC = () => (
-	<nav className={style()}>
-		<ul className={style('list')}>
-			<li className={style('element')}>
-				<Link to="/">Hello</Link>
-			</li>
-			<li className={style('element')}>
-				<Link to="/about">About</Link>
-			</li>
-		</ul>
-	</nav>
+	<header className={style()}>
+		<p className={style('title')}>Workbench screen: </p>
+		<h1 className={style('title', { 'is-main': true })}>
+			Silesian Amiga Classic Party
+		</h1>
+	</header>
 );
