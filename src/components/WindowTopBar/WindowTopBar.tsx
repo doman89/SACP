8021fe2bba00c44f8950f8186d56fa36ea883bb0
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2020-03-27 06:57:07 +0100

    feat(WindowTopBar): Top bar of Window with function to handle drag window and closing it

--- src/components/WindowTopBar/WindowTopBar.tsx
+++ src/components/WindowTopBar/WindowTopBar.tsx
@@ -1,17 +1,33 @@
 import React from 'react';
 import { default as bemCssModules } from 'bem-css-modules';
-import { WindowTopBarProps } from './interfaces/WindowTopBarProps';
-import { default as WindowTopBarStyles} from './WindowTopBar.module.scss';
 import { WindowButton } from '../WindowButton/WindowButton';
 import { WindowLabel } from '../WindowLabel/WindowLabel';
+import { WindowTopBarProps } from './interfaces/WindowTopBarProps';
+import { default as WindowTopBarStyles} from './WindowTopBar.module.scss';
 
 const style = bemCssModules(WindowTopBarStyles);
 
-export const WindowTopBar: React.FC<WindowTopBarProps> = ({isActive}) => (
+export const WindowTopBar: React.FC<WindowTopBarProps> = ({isActive, onClose, contentOf, handleOnMouseDown, handleOnMouseUp}) => (
 	<div className={style()}>
-		<WindowButton buttonIcon="is-close" isActive={isActive} />
-		<WindowLabel isActive={isActive} title="Workbench" />
-		<WindowButton buttonIcon="is-maximize" isActive={isActive} />
-		<WindowButton buttonIcon="is-change-window" isActive={isActive} />
+		<WindowButton 
+			buttonIcon="is-close"
+			contentOf={contentOf}
+			isActive={isActive}
+			onClick={onClose}
+		/>
+		<WindowLabel
+			handleOnMouseDown={handleOnMouseDown}
+			handleOnMouseUp={handleOnMouseUp}
+			isActive={isActive}
+			title={contentOf}
+		/>
+		<WindowButton
+			buttonIcon="is-maximize"
+			isActive={isActive}
+		/>
+		<WindowButton
+			buttonIcon="is-change-window"
+			isActive={isActive}
+		/>
 	</div>
 );
--- src/components/WindowTopBar/interfaces/WindowTopBarProps.ts
+++ src/components/WindowTopBar/interfaces/WindowTopBarProps.ts
@@ -1,3 +1,7 @@
 export interface WindowTopBarProps {
+	contentOf: string;
+	handleOnMouseDown:  (event: React.MouseEvent<HTMLElement, MouseEvent>) => void;
+	handleOnMouseUp: () => void;
 	isActive: boolean;
+	onClose: () => void;
 }
