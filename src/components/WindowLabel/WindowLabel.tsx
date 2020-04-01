157e8a6a7984334ea8ae01e4984e018a3ec48d79
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2020-04-01 20:53:07 +0200

    refactor(WindowLabel): Remove unnecessary handler for mouseUp event

--- src/components/WindowLabel/WindowLabel.tsx
+++ src/components/WindowLabel/WindowLabel.tsx
@@ -5,11 +5,10 @@ import { default as WindowLabelStyles } from './WindowLabel.module.scss';
 
 const style = bemCssModules(WindowLabelStyles);
 
-export const WindowLabel: React.FC<WindowLabelProps> = ({isActive, title, handleOnMouseDown, handleOnMouseUp}) => (
+export const WindowLabel: React.FC<WindowLabelProps> = ({isActive, title, handleOnMouseDown}) => (
 	<div
 		className={style({ 'is-active': isActive })}
 		onMouseDown={handleOnMouseDown}
-		onMouseUp={handleOnMouseUp}
 	>
 		<p className={style('title')}>
 			{title}
