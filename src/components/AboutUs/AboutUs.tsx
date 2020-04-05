3f7a6893d8552d68384c4017b35adef550b88b94
Mateusz Domanski <mateusz.domanski.1989@gmail.com>
2020-04-05 15:53:24 +0200

    feat(AboutUs): Example data for view how Window component works

--- src/components/AboutUs/AboutUs.tsx
+++ src/components/AboutUs/AboutUs.tsx
@@ -1,17 +1,51 @@
 import React from 'react';
 import { Window } from './../Window/Window';
+import { WithWindowContext } from '../../contexts/WindowContext/interfaces/WithWindowContext';
+import { withWindowContext } from '../../contexts/WindowContext/withWindowContext';
 
-interface AboutUsProps {
+interface AboutUsProps extends WithWindowContext{
 	contentOf: string;
 	leftPosition: number;
 	topPosition: number;
 }
 
-export const AboutUs: React.FC<AboutUsProps> = (props) => {
+const AboutUs: React.FC<AboutUsProps> = (props) => {
+	if (props.context.websiteData === null) {
+		return null;
+	}
+
+	const { aboutUs } = props.context.websiteData;
+
 	return (
 		<Window {...props}>
-			<h2>O nas</h2>
-			<p>Lorem ipsum dolor</p>
+			<div style={{width: '800px'}}>
+				{aboutUs.sectionDescription}
+				<ul>
+					{aboutUs.members.map(member => (
+						<li key={member.nickname}>
+							<p>
+								{member.nickname}
+							</p>
+							<div>
+								<img alt={member.nickname} src={member.photo} />
+							</div>
+							<ul>
+								{member.equipment.map(thing => <li key={thing}>{thing}</li>)}
+							</ul>
+							<ul>
+								{member.hobbies.map(hobby => <li key={hobby}>{hobby}</li>)}
+							</ul>
+							<p>
+								{member.contact}
+							</p>
+						</li>
+					))}
+				</ul>
+			</div>
 		</Window>
 	)
-}
+};
+
+const AboutUsConsumer = withWindowContext(AboutUs);
+
+export { AboutUsConsumer as AboutUs };
